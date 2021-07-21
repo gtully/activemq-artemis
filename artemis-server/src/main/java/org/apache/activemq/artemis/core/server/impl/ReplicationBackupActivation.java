9a62e3dfb8fc79ac460200cb25d9b1adc7177861
gtully <gary.tully@gmail.com>
2021-07-21 13:41:23 +0100

    ARTEMIS-2716 - speed up failback backup behaviour by ignoring existing data when appropriate and have backup wait for activation before restarting as backup after failback which avoids a race

--- artemis-server/src/main/java/org/apache/activemq/artemis/core/server/impl/ReplicationBackupActivation.java
+++ artemis-server/src/main/java/org/apache/activemq/artemis/core/server/impl/ReplicationBackupActivation.java
@@ -150,26 +150,25 @@ public final class ReplicationBackupActivation extends Activation implements Dis
          LOGGER.infof("Trying to reach the majority of quorum nodes");
          distributedManager.start();
          LOGGER.debug("Quorum service available");
-         distributedManager.addUnavailableManagerListener(this);
 
-         // we may be a valid insync_replica (backup) if our activation sequence is largest for a nodeId
-         // verify that before removing data..
-
-         // however we may also be a backup activation ofter a failback request
-         // in this case, we don't want to take the lock to stop the the failback
-         // there is a delay in
-         // org.apache.activemq.artemis.core.server.impl.ReplicationPrimaryActivation.restartAsBackupAfterFailback
-         final DistributedLock liveLockWithInSyncReplica = checkForInSyncReplica();
-         if (liveLockWithInSyncReplica != null) {
-            // retain state and start as live
-            if (!activeMQServer.initialisePart1(false)) {
+         if (isFirstFailbackAttempt()) {
+            // if we are replicating such that we can request failback, we can ignore our data
+            // on a hard restart we would be back to primary role
+         } else {
+            // we may be a valid insync_replica (backup) if our activation sequence is largest for a nodeId
+            // verify that before removing data..
+            final DistributedLock liveLockWithInSyncReplica = checkForInSyncReplica();
+            if (liveLockWithInSyncReplica != null) {
+               // retain state and start as live
+               if (!activeMQServer.initialisePart1(false)) {
+                  return;
+               }
+               activeMQServer.setState(ActiveMQServerImpl.SERVER_STATE.STARTED);
+               startAsLive(liveLockWithInSyncReplica);
                return;
             }
-            activeMQServer.setState(ActiveMQServerImpl.SERVER_STATE.STARTED);
-            startAsLive(liveLockWithInSyncReplica);
-            return;
          }
-
+         distributedManager.addUnavailableManagerListener(this);
          // Stop the previous node manager and create a new one with NodeManager::replicatedBackup == true:
          // NodeManager::start skip setup lock file with NodeID, until NodeManager::stopBackup is called.
          activeMQServer.resetNodeManager();
